5a28d0e21b4f6c852e8ca1519d1c69e333959c5a
Aladix002 <xbotlo01@stud.fit.vut.cz>
2025-05-09 17:42:42 +0200

    bez zbytocneho publishovania

--- cpp_project_template/src/nodes/camera.cpp
+++ cpp_project_template/src/nodes/camera.cpp
@@ -2,15 +2,12 @@
 #include "std_msgs/msg/int32.hpp"
 
 CameraNode::CameraNode()
-    : Node("camera_node"), it_(std::make_shared<rclcpp::Node>("camera_node")) {
+    : Node("camera_node_" + std::to_string(rand())), it_(std::make_shared<rclcpp::Node>("camera_node")) {
 
     // Image subscriber to receive the camera images
     image_subscriber_ = this->create_subscription<sensor_msgs::msg::CompressedImage>(
         "/bpc_prp_robot/camera/compressed", 10,
         std::bind(&CameraNode::image_callback, this, std::placeholders::_1));
-
-    // Image publisher for processed camera images
-    image_publisher_ = it_.advertise("/bpc_prp_robot/camera/processed", 1);
 
     // ArUco ID publisher to send the last detected ArUco ID
     aruco_id_publisher_ = this->create_publisher<std_msgs::msg::Int32>("/bpc_prp_robot/tag_detected", 10);
@@ -44,13 +41,6 @@ void CameraNode::image_callback(const sensor_msgs::msg::CompressedImage::SharedP
             aruco_msg.data = last_aruco_id_;
             aruco_id_publisher_->publish(aruco_msg);
         }
-
-        // Convert the processed image to a ROS image message
-        sensor_msgs::msg::Image::SharedPtr processed_image_msg =
-            cv_bridge::CvImage(std_msgs::msg::Header(), "bgr8", frame).toImageMsg();
-
-        // Publish the processed image
-        image_publisher_.publish(processed_image_msg);
 
     } catch (const cv_bridge::Exception &e) {
         RCLCPP_ERROR(this->get_logger(), "Error converting image: %s", e.what());
